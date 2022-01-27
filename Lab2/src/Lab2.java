1a4d00c3331e7363e96594bf80e61356d00c7317
Alex Anderson <ama7019@psu.edu>
2022-01-27 17:58:44 -0500

    Logic completed. Print statements unsure if correct

--- Lab2/src/Lab2.java
+++ Lab2/src/Lab2.java
@@ -3,20 +3,22 @@ public class Lab2 {
     private static int count = 0;
 
    public static double function(double x) {
-       return 9 * Math.pow(x,6) + 3 * Math.pow(x,5) + 4 * Math.pow(x,4) + 2 * Math.pow(x,3) + 2 * Math.pow(x,2) + 9 * x + 1;
+       return (9 * Math.pow(x,6) + 3 * Math.pow(x,5) + 4 * Math.pow(x,4) + 2 * Math.pow(x,3) + 2 * Math.pow(x,2) + 9 * x + 1);
    }
 
    public static double derivativeFunction(double x) {
-       return 54 * Math.pow(x,5) + 15 * Math.pow(x,4) + 16 * Math.pow(x,3) + 6 * Math.pow(x,2) + 4 * Math.pow(x,1) + 9;
+       return (54 * Math.pow(x,5) + 15 * Math.pow(x,4) + 16 * Math.pow(x,3) + 6 * Math.pow(x,2) + 4 * Math.pow(x,1) + 9);
    }
 
     public static void newtonRaphson(double x) {
-        double h = function(x) / derivativeFunction(x);;
+        double h = function(x) / derivativeFunction(x);
         while (Math.abs(h) >= EPSILON && count <= 2000) {
             h = function(x) / derivativeFunction(x);
             count++;
+            double guessedX = x;
             x = x - h;
-            System.out.println("Iteration: " + count + " Absolute Root: " + x + " Rounded Root: " + Math.round(x) * 100);
+            System.out.println("Iteration: " + count + " |Guessed x value: " + Math.round(guessedX * 100.0) / 100.0
+                    + " |Derived Root: " + Math.round(x * 100.0) / 100.0 + " |Polynomial Value: " + Math.round(function(x) * 100.0) / 100.0);
         }
         if(count >= 2000) {
             System.out.println("Roots not found");
@@ -24,7 +26,7 @@ public class Lab2 {
     }
 
     public static void main(String[] args) {
-        double x0 = 2;
+        double x0 = 300;
         newtonRaphson(x0);
     }
 
